07ddee4dff57d8494d0ad477cd273297b383db08
frost-head <ed.ayush2003@gmail.com>
2022-04-04 16:10:12 +0530

    Added database file

--- src/main.py
+++ src/main.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
+import os
+from database import *
+
 
 app = Flask(__name__)
+mysql = MySQL(app)
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = os.environ.get("Mysql_user")
+app.config['MYSQL_PASSWORD'] = os.environ.get("Mysql_pass")
+app.config['MYSQL_DB'] = 'hospital_managment'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.secret_key = os.urandom(24)
+
 
 @app.route('/')
 def home():
     return "<h1>Project Setup</h1>"
+
 
 app.run(debug=True, host='0.0.0.0')
