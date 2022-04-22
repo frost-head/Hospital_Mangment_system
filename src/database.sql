0c5757cbb218161a81b201c2b8708122a6816794
Dev <devbh789@gmail.com>
2022-04-22 11:24:07 +0530

    Changes in database

--- src/database.sql
+++ src/database.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
--- Host: localhost    Database: hospital
+-- Host: localhost    Database: hospital_managment
 -- ------------------------------------------------------
 -- Server version	8.0.28
 
@@ -16,6 +16,55 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `appointments`
+--
+
+DROP TABLE IF EXISTS `appointments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `appointments` (
+  `app_id` int NOT NULL AUTO_INCREMENT,
+  `pid` int DEFAULT NULL,
+  `sid` int DEFAULT NULL,
+  `date_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`app_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `appointments`
+--
+
+LOCK TABLES `appointments` WRITE;
+/*!40000 ALTER TABLE `appointments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `appointments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cart` (
+  `item_id` int NOT NULL,
+  `pid` int DEFAULT NULL,
+  `qty` int DEFAULT NULL,
+  PRIMARY KEY (`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart`
+--
+
+LOCK TABLES `cart` WRITE;
+/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `patient`
 --
 
@@ -27,12 +76,13 @@ CREATE TABLE `patient` (
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
+  `number` bigint DEFAULT NULL,
   `address` varchar(255) DEFAULT NULL,
   `age` int DEFAULT NULL,
-  `blood_group` varchar(3) DEFAULT NULL,
+  `blood_group` varchar(255) DEFAULT NULL,
   `father_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`pid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,6 +91,7 @@ CREATE TABLE `patient` (
 
 LOCK TABLES `patient` WRITE;
 /*!40000 ALTER TABLE `patient` DISABLE KEYS */;
+INSERT INTO `patient` VALUES (1,'devbh789@gmail.com','$2b$12$7NuqMKh1PSLk/iNA1VH1.uQwxVdeqtS7PykMwm2lNlHHjuYVC8cki','Dev',0,'adads',19,'O+','dsadasd');
 /*!40000 ALTER TABLE `patient` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -73,6 +124,32 @@ LOCK TABLES `staff` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `store`
+--
+
+DROP TABLE IF EXISTS `store`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `store` (
+  `item_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `qty` bigint DEFAULT NULL,
+  `price` int DEFAULT NULL,
+  PRIMARY KEY (`item_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `store`
+--
+
+LOCK TABLES `store` WRITE;
+/*!40000 ALTER TABLE `store` DISABLE KEYS */;
+INSERT INTO `store` VALUES (1,'Ecosprin 75mg',10000,4),(2,'Shelcal 500mg',5696,98);
+/*!40000 ALTER TABLE `store` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `vitals`
 --
 
@@ -86,11 +163,10 @@ CREATE TABLE `vitals` (
   `blood_pressure` int DEFAULT NULL,
   `resp_rate` int DEFAULT NULL,
   `spo2` int DEFAULT NULL,
-  `datetime` date DEFAULT NULL,
-  `sid` int NOT NULL,
+  `datetime` datetime DEFAULT NULL,
+  `sid` int DEFAULT NULL,
   `weight` int DEFAULT NULL,
-  PRIMARY KEY (`pid`),
-  CONSTRAINT `vitals_ibfk_1` FOREIGN KEY (`pid`) REFERENCES `patient` (`pid`)
+  PRIMARY KEY (`pid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -112,4 +188,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-02 17:26:23
+-- Dump completed on 2022-04-22 11:22:08
