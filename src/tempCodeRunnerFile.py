ba11b1bd712ff2909ad8424736ad768e263577b8
Dev <devbh789@gmail.com>
2022-05-10 10:58:39 +0530

    Changes in cart

--- src/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-
-        #     qty += old_qty
-        #     insert(mysql, 'update cart set qty = {} where item_id = {} and pid = {}'.format(
-        #         qty, item_id, session['user']))
-        #     return storeRedirect(item_id, qty)
-        # else:
-        #     return storeRedirect(item_id, qty)
